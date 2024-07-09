9baa949f6e602b766eab1db6888fc8e73da9c299
Christian John jiménez Calvo <christianjjc@mail.com>
2024-07-08 19:11:43 -0500

    create pokemon ok con seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import axios, { AxiosInstance } from 'axios';
 import { PokRepsonse } from './interfaces/pokemon-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   public async executeSeed() {
-    const { data } = await this.axios.get<PokRepsonse>('https://pokeapi.co/api/v2/pokemon?limit=5');
-    data.results.forEach(({ name, url }) => {
+    await this.pokemonModel.deleteMany();
+    const { data } = await this.axios.get<PokRepsonse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+    data.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = Number(segments[segments.length - 2]);
-      console.log({ name, no });
+      await this.pokemonModel.create({ name, no });
     });
-    return data.results;
+    return 'Seed Executed!';
   }
 }
